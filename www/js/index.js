558840bf6773ca3d3768cf5a0eba34f8da1ffc8b
Gao WanQian <gaowanqian.99@gmail.com>
2021-07-11 14:34:33 +0800

    v3, completed home page, home.html

--- www/js/index.js
+++ www/js/index.js
@@ -28,22 +28,74 @@ function onDeviceReady() {
     document.getElementById('deviceready').classList.add('ready');
 }
 
+
+
+
 var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
+
+
+
+
+
+function openForm() {
+    /*displays form, and dont display chat icon button,  when form opens*/
+    document.getElementById("myForm").style.display = "block";
+    document.getElementById("chat_icon").style.display = "none";
+    document.getElementById("trainerchatmsg").style.display = "none";
+    document.getElementById("nutritionistchatmsg").style.display = "none";
+}
+
+function closeForm() {
+    /*displays chat icon button, and dont display form,  when form closes*/
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("chat_icon").style.display = "block";
+    
+    /*reset the select chat type screen when form closes*/
+    document.getElementById("chattxt").style.display = "block";
+    document.getElementById("seltype").style.display = "block";
+    document.getElementById("trainerChat").style.display = "block";
+    document.getElementById("nutritionistChat").style.display = "block";
+    document.getElementById("chatfaq").style.display = "block";
+}
+
+function trainer() {
+    /*remove visibility of select chat type screen when user selects a chat type*/
+    document.getElementById("trainerchatmsg").style.display = "block";
+    document.getElementById("nutritionistchatmsg").style.display = "none";
+    document.getElementById("chattxt").style.display = "none";
+    document.getElementById("seltype").style.display = "none";
+    document.getElementById("trainerChat").style.display = "none";
+    document.getElementById("nutritionistChat").style.display = "none";
+    document.getElementById("chatfaq").style.display = "none";
+}
+
+function nutritionist() {
+    /*remove visibility of select chat type screen when user selects a chat type*/
+    document.getElementById("nutritionistchatmsg").style.display = "block";
+    document.getElementById("trainerchatmsg").style.display = "none";
+    document.getElementById("chattxt").style.display = "none";
+    document.getElementById("seltype").style.display = "none";
+    document.getElementById("trainerChat").style.display = "none";
+    document.getElementById("nutritionistChat").style.display = "none";
+    document.getElementById("chatfaq").style.display = "none";
 }
